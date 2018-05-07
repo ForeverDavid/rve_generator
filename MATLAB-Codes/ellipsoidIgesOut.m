9d27dd7503094377669ccffcc67cd7ad8c81c04c
Li <xli@ibf-aachen.local>
2018-05-07 13:52:16 +0200

    matlab code debug

--- MATLAB-Codes/ellipsoidIgesOut.m
+++ MATLAB-Codes/ellipsoidIgesOut.m
@@ -1,5 +1,5 @@
 function ellipsoidIgesOut(x, y, z, outName)
-kurve = nrbcirc(1,[0,0,0], 0, pi);
+kurve = nrbcirc(0.5,[0,0,0], 0, pi);
 oberflaeche_Kugel = nrbrevolve(kurve, [0,0,0], [1,0,0], 2*pi);
 oberflaeche_Ellipsoid = nrbtform(oberflaeche_Kugel, vecscale([x, y, z]));
 igesout(oberflaeche_Ellipsoid, outName)
