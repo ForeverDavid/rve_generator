a7696b179c30686c785a34faa379bd037467e330
sululee <sululee@foxmail.com>
2018-05-07 13:35:11 +0200

    MATLAB Codes

---
+++ MATLAB-Codes/ellipsoidIgesOut.m
@@ -0,0 +1,6 @@
+function ellipsoidIgesOut(x, y, z, outName)
+kurve = nrbcirc(1,[0,0,0], 0, pi);
+oberflaeche_Kugel = nrbrevolve(kurve, [0,0,0], [1,0,0], 2*pi);
+oberflaeche_Ellipsoid = nrbtform(oberflaeche_Kugel, vecscale([x, y, z]));
+igesout(oberflaeche_Ellipsoid, outName)
+display('fertig')
